771a7d651286975f0e5df861cc6b05ce9fe0304d
JC Abanto <74562452+jobcabanto@users.noreply.github.com>
2023-02-20 23:34:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ https://scikit-learn.org/stable/auto_examples/linear_model/plot_logistic.html#sp
 
 """
 
-from sklearn.preprocessing import OrdinalEncoder, Normalizer, MinMaxScaler
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-import pandas as pd, numpy as np, seaborn as sns, matplotlib.pyplot as plt, csv
+from xgboost import XGBClassifier
+import pandas as pd, csv
 
 class PSWorkshopQualifiers(object):
     
@@ -16,60 +17,60 @@ class PSWorkshopQualifiers(object):
         # Object-oriented programming variable initialization
 
         self.scoreset, self.trainset = pd.read_csv(score), pd.read_csv(train)
-        self.features, self.target = [], []
+        self.features, self.target, self.scoringFeatures = [], [], []
 
-    def prepareData(self, ordEnc = OrdinalEncoder()):
+    def prepareTrainSet(self, ordEnc = OrdinalEncoder()):
 
         # Quantifying categorical variables
     
-        self.scoreset["Gender"] = ordEnc.fit_transform(self.scoreset[["Gender"]])
-        self.scoreset["hadVehicleClaimInPast"] = ordEnc.fit_transform(self.scoreset[["hadVehicleClaimInPast"]])
-        self.scoreset["hasMortgage"] = ordEnc.fit_transform(self.scoreset[["hasMortgage"]])
-        self.scoreset["vehicleStatus"] = ordEnc.fit_transform(self.scoreset[["vehicleStatus"]])
         self.trainset["Gender"] = ordEnc.fit_transform(self.trainset[["Gender"]])
         self.trainset["hadVehicleClaimInPast"] = ordEnc.fit_transform(self.trainset[["hadVehicleClaimInPast"]])
-        self.trainset["hasMortgage"] = ordEnc.fit_transform(self.trainset[["hasMortgage"]])
         self.trainset["vehicleStatus"] = ordEnc.fit_transform(self.trainset[["vehicleStatus"]])
-        print(self.scoreset)
-        print(self.trainset)
+        self.scoreset["Gender"] = ordEnc.fit_transform(self.scoreset[["Gender"]])
+        self.scoreset["hadVehicleClaimInPast"] = ordEnc.fit_transform(self.scoreset[["hadVehicleClaimInPast"]])
+        self.scoreset["vehicleStatus"] = ordEnc.fit_transform(self.scoreset[["vehicleStatus"]])
 
         # Normalizing all quantitative variables
 
-        """
-        
-        Write a for loop iterating through variables and use Normalize().fit_transform(i) function 
-
-        """
+        self.trainset = pd.DataFrame(MinMaxScaler().fit_transform(self.trainset.values))
+        self.scoreset = pd.DataFrame(MinMaxScaler().fit_transform(self.scoreset.values))
 
         return self.scoreset, self.trainset
 
     def unpackData(self):
 
-        # Creating training and test sets*
+        # Extracting from Training Set
 
         for i in range(len(self.trainset)):
             tempFeatures = [self.trainset.iloc[i][0]]
-            self.target.append = self.trainset.iloc[i][1]
-            for j in range(2, 15):
+            self.target.append(self.trainset.iloc[i][1])
+            for j in range(2, 14):
                 tempFeatures.append(self.trainset.iloc[0][j])
             self.features.append(tempFeatures)
 
-        return self.features, self.target
+        # Extracting from Scoring Set
 
-    def modelData(self, c = 1e5):
+        for i in range(len(self.scoreset)):
+            tempFeatures = [self.scoreset.iloc[i][0]]
+            for j in range(1, 13):
+                tempFeatures.append(self.scoreset.iloc[0][j])
+            self.scoringFeatures.append(tempFeatures)
 
-        # Applying linear and logistic regression the various sets*
+        return self.features, self.target, self.scoringFeatures
 
-        X_train, X_test, Y_train, Y_test = train_test_split(self.features, self.target, test_size = 0.25, random_state = None)
-        clf = LogisticRegression(c)
-        clf.fit(X_train, Y_train)
-        ols = LinearRegression()
-        ols.fit(X_train, Y_train)
-        pass
+    def modelData(self):
 
+        # Applying logistic regression the various sets*
+
+        X_train, X_test, Y_train, Y_test = train_test_split(self.features, self.target, test_size = 0.20, random_state = None)
+        model = XGBClassifier(objective ='binary:logistic', random_state = None)
+        model.fit(X_train, Y_train)
+        print(classification_report(Y_test, model.predict(X_test)))
+        print(model.predict(self.scoringFeatures))
+        
     def convertData(self):
 
-        # Write predictions into .csv file
+        # Write predictions into .csv file*
 
         with open("", 'w') as file:
             writer = csv.writer(file)
@@ -77,6 +78,6 @@ class PSWorkshopQualifiers(object):
 
 client = PSWorkshopQualifiers(r"C:\Users\JC\OneDrive\Documents\University - Level II\mathage\ScoringDataset_2023Qualification.csv", 
                             r"C:\Users\JC\OneDrive\Documents\University - Level II\mathage\TrainingDataset_2023Qualification.csv")
-client.prepareData()
-
-
+client.prepareTrainSet()
+client.unpackData()
+client.modelData()
